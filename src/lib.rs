b50a821ec970c3d742893bf75d8475b121690241
Marvin Löbel <loebel.marvin@gmail.com>
2021-02-26 00:27:05 +0100

    initial working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,50 @@
-use std::sync::Mutex;
+use std::sync::{
+    mpsc::{channel, Receiver, Sender},
+    Mutex,
+};
 use std::{ops::Deref, sync::Arc};
 
 type Buf<T> = Arc<T>;
 struct ReadUpdate<T> {
-    shared: Arc<Mutex<Buf<T>>>,
+    shared: Arc<Mutex<Option<Buf<T>>>>,
 }
 impl<T> ReadUpdate<T> {
-    fn new(v: Buf<T>) -> Self {
+    fn new() -> Self {
         Self {
-            shared: Arc::new(Mutex::new(v)),
+            shared: Arc::new(Mutex::new(None)),
         }
     }
-    fn replace(&self, v: Buf<T>) -> Buf<T> {
-        std::mem::replace(&mut self.shared.lock().unwrap(), v)
+    fn replace(&self, v: Buf<T>) -> Option<Buf<T>> {
+        std::mem::replace(&mut self.shared.lock().unwrap(), Some(v))
     }
-    fn get(&self) -> Buf<T> {
-        self.shared.lock().unwrap().clone()
+    fn get(&self) -> Option<Buf<T>> {
+        self.shared.lock().unwrap().take()
     }
 }
 
 pub struct Writer<T> {
-    prev_buf: Buf<T>,
     make_buf: Box<dyn FnMut(&T) -> T>,
-    unused_bufs: Vec<Buf<T>>,
+    unused_bufs_rx: Receiver<Buf<T>>,
+
+    prev_buf: Buf<T>,
+    unused_bufs_tx: Sender<Buf<T>>,
     read_update: ReadUpdate<T>,
 }
 
 pub struct Reader<T> {
+    prev_buf: Buf<T>,
+    unused_bufs_tx: Sender<Buf<T>>,
     read_update: ReadUpdate<T>,
 }
 
-pub fn new_buffer<T>(init: T, make_buf: impl FnMut(&T) -> T + 'static) -> (Writer<T>, Reader<T>) {
+pub fn new_buffer_with<T>(
+    init: T,
+    make_buf: impl FnMut(&T) -> T + 'static,
+) -> (Writer<T>, Reader<T>) {
     let w = Writer::new(init, make_buf);
     let r = Reader {
+        prev_buf: w.prev_buf.clone(),
+        unused_bufs_tx: w.unused_bufs_tx.clone(),
         read_update: ReadUpdate {
             shared: w.read_update.shared.clone(),
         },
@@ -40,28 +52,33 @@ pub fn new_buffer<T>(init: T, make_buf: impl FnMut(&T) -> T + 'static) -> (Write
     (w, r)
 }
 
+pub fn new_buffer_clone<T: Clone>(init: T) -> (Writer<T>, Reader<T>) {
+    new_buffer_with(init, |v| v.clone())
+}
+
 impl<T> Writer<T> {
     fn new(init: T, make_buf: impl FnMut(&T) -> T + 'static) -> Self {
         let prev_buf = Arc::new(init);
         let make_buf = Box::new(make_buf);
-        let read_update = ReadUpdate::new(prev_buf.clone());
+        let read_update = ReadUpdate::new();
+        let (unused_bufs_tx, unused_bufs_rx) = channel();
         Self {
             prev_buf,
             make_buf,
-            unused_bufs: Vec::new(),
+            unused_bufs_tx,
+            unused_bufs_rx,
             read_update,
         }
     }
 
     fn get_unused_buffer(&mut self) -> Buf<T> {
-        if let Some(buf) = self.unused_bufs.pop() {
+        if let Some(buf) = self.unused_bufs_rx.try_recv().ok() {
+            debug_assert!(Arc::strong_count(&buf) == 1);
+            debug_assert!(Arc::weak_count(&buf) == 0);
             return buf;
         }
         let new_state = (self.make_buf)(&self.prev_buf);
-        let buf = Arc::new(new_state);
-        debug_assert!(Arc::strong_count(&buf) == 1);
-        debug_assert!(Arc::weak_count(&buf) == 0);
-        buf
+        Arc::new(new_state)
     }
 
     pub fn update(&mut self, mut write_op: impl FnMut(&T, &mut T)) {
@@ -72,28 +89,130 @@ impl<T> Writer<T> {
         let mut_ref = Arc::get_mut(&mut new_state).unwrap();
         write_op(&self.prev_buf, mut_ref);
 
-        let unused_old_read = self.read_update.replace(new_state.clone());
-        self.prev_buf = new_state;
-        self.unused_bufs.push(unused_old_read);
-    }
-}
-
-impl<T> Reader<T> {
-    pub fn get_newest(&self) -> ReadState<T> {
-        ReadState {
-            buf: self.read_update.get(),
+        self.prev_buf = new_state.clone();
+        if let Some(unused_buf) = self.read_update.replace(new_state) {
+            self.unused_bufs_tx.send(unused_buf).unwrap();
         }
     }
 }
 
-pub struct ReadState<T> {
-    buf: Buf<T>,
+impl<T> Reader<T> {
+    pub fn newest(&mut self) -> ReadState<'_, T> {
+        match self.read_update.get() {
+            Some(new_buf) => {
+                let now_unused_buf = std::mem::replace(&mut self.prev_buf, new_buf);
+                self.unused_bufs_tx.send(now_unused_buf).unwrap();
+                ReadState {
+                    buf: &self.prev_buf,
+                }
+            }
+            None => ReadState {
+                buf: &self.prev_buf,
+            },
+        }
+    }
 }
 
-impl<T> Deref for ReadState<T> {
+pub struct ReadState<'a, T> {
+    buf: &'a Buf<T>,
+}
+
+impl<T> Deref for ReadState<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn measure() -> [Arc<Mutex<usize>>; 2] {
+        let p = Arc::new(Mutex::new(0));
+        [p.clone(), p]
+    }
+
+    fn count(ptr: &Arc<Mutex<usize>>) {
+        *ptr.lock().unwrap() += 1;
+    }
+
+    fn final_count(ptr: &Arc<Mutex<usize>>) -> usize {
+        *ptr.lock().unwrap()
+    }
+
+    #[test]
+    fn test_seq_1() {
+        let [c, c2] = measure();
+
+        let (mut w, mut r) = new_buffer_with(0, move |i| {
+            count(&c2);
+            *i
+        });
+        assert_eq!(*r.newest(), 0);
+        w.update(|old, new| {
+            *new = *old + 1;
+        });
+        assert_eq!(*r.newest(), 1);
+        assert!(final_count(&c) <= 2);
+    }
+
+    #[test]
+    fn test_long_overlapping_read() {
+        let [c, c2] = measure();
+
+        let (mut w, mut r) = new_buffer_with(0, move |i| {
+            count(&c2);
+            *i
+        });
+        {
+            let r = r.newest();
+            assert_eq!(*r, 0);
+            w.update(|old, new| {
+                *new = *old + 1;
+            });
+            assert_eq!(*r, 0);
+            w.update(|old, new| {
+                *new = *old + 1;
+            });
+            assert_eq!(*r, 0);
+            w.update(|old, new| {
+                *new = *old + 1;
+            });
+            assert_eq!(*r, 0);
+            w.update(|old, new| {
+                *new = *old + 1;
+            });
+            assert_eq!(*r, 0);
+            w.update(|old, new| {
+                *new = *old + 1;
+            });
+            assert_eq!(*r, 0);
+        }
+        assert_eq!(*r.newest(), 5);
+        assert!(final_count(&c) <= 2);
+    }
+
+    #[test]
+    fn test_long_overlapping_write() {
+        let [c, c2] = measure();
+
+        let (mut w, mut r) = new_buffer_with(0, move |i| {
+            count(&c2);
+            *i
+        });
+
+        w.update(|old, new| {
+            assert_eq!(*r.newest(), 0);
+            assert_eq!(*r.newest(), 0);
+            assert_eq!(*r.newest(), 0);
+            assert_eq!(*r.newest(), 0);
+            assert_eq!(*r.newest(), 0);
+            *new = *old + 1;
+        });
+        assert_eq!(*r.newest(), 1);
+
+        assert!(final_count(&c) <= 2);
     }
 }
